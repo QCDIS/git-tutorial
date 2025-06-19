54bc9fd2b57208d2bdc067107a0c3cab1fae636a
skoulouzis <koulouzis@gmail.com>
2025-06-19 09:56:09 +0200

    args

--- app/main.py
+++ app/main.py
@@ -7,8 +7,54 @@ from select_habitat import select_habitat
 
 arg_parser = argparse.ArgumentParser()
 
+arg_parser.add_argument(
+    '--habitat_name',
+    action='store',
+    type=str,
+    required=True,
+    dest='habitat_name',
+    choices=[
+        'forests',
+        'open_forests',
+        'scrub',
+        'natural_grasslands',
+        'human_maintained_grasslands',
+        'wetland',
+        'ruderal_habitats',
+        'agricultural_habitat'
+    ],
+    help='Name of the habitat. Choices: forests, open_forests, scrub, natural_grasslands, human_maintained_grasslands, wetland, ruderal_habitats, agricultural_habitat'
+)
+arg_parser.add_argument(
+    '--param_climate_model'
+    , action='store',
+    type=str,
+    required=False,
+    dest='param_climate_model',
+    default='IPSL-CM6A-LR',
+    choices=[
+        'Current',
+        'GFDL-ESM4',
+        'IPSL-CM6A-LR',
+        'MPI-ESM1-2-HR',
+        'MRI-ESM2-0',
+        'UKESM1-0-LL',
+        'Ensemble'
+    ],
+    help='Climate model to use for the simulation. Choices: Current, GFDL-ESM4, IPSL-CM6A-LR, MPI-ESM1-2-HR, MRI-ESM2-0, UKESM1-0-LL, Ensemble'
+)
 
-arg_parser.add_argument('--habitat_name', action='store', type=str, required=True, dest='habitat_name')
+
+arg_parser.add_argument(
+    '--param_species_class'
+    , action='store',
+    type=str,
+    required=False,
+    dest='param_species_class',
+    default='all',
+    choices=[ 'Liliopsida', 'Magnoliopsida', 'Pinopsida'],
+    help='Species class to use for the simulation. Choices: Liliopsida, Magnoliopsida, Pinopsida'
+)
 
 args = arg_parser.parse_args()
 print(args)
@@ -20,8 +66,8 @@ df_hab = pandas.read_csv(url)
 
 habitat_type = habitat_name.replace(' ','_').lower()
 selected_hab_abb = str(df_hab[df_hab["hab_name"] == habitat_type]["hab_abb"].values[0])
-param_climate_model = 'IPSL-CM6A-LR'
-param_species_class = 'Liliopsida'
+param_climate_model = args.param_climate_model
+param_species_class = args.param_species_class
 
 
 conf_x =  0.95
